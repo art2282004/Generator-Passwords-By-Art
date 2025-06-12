125704428e4c714c765d9dd3491ba250a860abed
art2282004 <107762461+art2282004@users.noreply.github.com>
2025-06-12 03:23:32 +0300

    v1.1
    Added Range Of Characters

--- mainform.h
+++ mainform.h
@@ -52,6 +52,19 @@ namespace GeneratorPasswordsByArt {
 	private: System::Windows::Forms::TextBox^ NumberOfLinesEnter;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Button^ CopyResultButton;
+	private: System::Windows::Forms::CheckBox^ RangeNumbers;
+	private: System::Windows::Forms::OpenFileDialog^ openFileDialog1;
+	private: System::Windows::Forms::TextBox^ FromTextBox;
+	private: System::Windows::Forms::TextBox^ ToTextBox;
+	private: System::Windows::Forms::Label^ FromLabel;
+	private: System::Windows::Forms::Label^ ToLabel;
+
+
+
+
+
+
+
 
 
 
@@ -85,6 +98,12 @@ namespace GeneratorPasswordsByArt {
 			this->NumberOfLinesEnter = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->CopyResultButton = (gcnew System::Windows::Forms::Button());
+			this->RangeNumbers = (gcnew System::Windows::Forms::CheckBox());
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->FromTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->ToTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->FromLabel = (gcnew System::Windows::Forms::Label());
+			this->ToLabel = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// GenerateButton
@@ -139,7 +158,7 @@ namespace GeneratorPasswordsByArt {
 			// 
 			// NumberEnter
 			// 
-			this->NumberEnter->Location = System::Drawing::Point(47, 6);
+			this->NumberEnter->Location = System::Drawing::Point(47, 9);
 			this->NumberEnter->Name = L"NumberEnter";
 			this->NumberEnter->Size = System::Drawing::Size(90, 20);
 			this->NumberEnter->TabIndex = 6;
@@ -191,23 +210,81 @@ namespace GeneratorPasswordsByArt {
 			this->CopyResultButton->Visible = false;
 			this->CopyResultButton->Click += gcnew System::EventHandler(this, &mainform::CopyResultButton_Click);
 			// 
+			// RangeNumbers
+			// 
+			this->RangeNumbers->AutoSize = true;
+			this->RangeNumbers->Location = System::Drawing::Point(348, 8);
+			this->RangeNumbers->Name = L"RangeNumbers";
+			this->RangeNumbers->Size = System::Drawing::Size(214, 17);
+			this->RangeNumbers->TabIndex = 12;
+			this->RangeNumbers->Text = L"Enable Range Characters In Passwords";
+			this->RangeNumbers->UseVisualStyleBackColor = true;
+			this->RangeNumbers->CheckedChanged += gcnew System::EventHandler(this, &mainform::RangeNumbers_CheckedChanged);
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			// 
+			// FromTextBox
+			// 
+			this->FromTextBox->Location = System::Drawing::Point(33, 9);
+			this->FromTextBox->Name = L"FromTextBox";
+			this->FromTextBox->Size = System::Drawing::Size(40, 20);
+			this->FromTextBox->TabIndex = 13;
+			this->FromTextBox->Visible = false;
+			this->FromTextBox->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &mainform::CheckDigit);
+			// 
+			// ToTextBox
+			// 
+			this->ToTextBox->Location = System::Drawing::Point(97, 9);
+			this->ToTextBox->Name = L"ToTextBox";
+			this->ToTextBox->Size = System::Drawing::Size(40, 20);
+			this->ToTextBox->TabIndex = 14;
+			this->ToTextBox->Visible = false;
+			this->ToTextBox->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &mainform::CheckDigit);
+			// 
+			// FromLabel
+			// 
+			this->FromLabel->AutoSize = true;
+			this->FromLabel->Location = System::Drawing::Point(6, 12);
+			this->FromLabel->Name = L"FromLabel";
+			this->FromLabel->Size = System::Drawing::Size(30, 13);
+			this->FromLabel->TabIndex = 10;
+			this->FromLabel->Text = L"From";
+			this->FromLabel->Visible = false;
+			// 
+			// ToLabel
+			// 
+			this->ToLabel->AutoSize = true;
+			this->ToLabel->Location = System::Drawing::Point(75, 12);
+			this->ToLabel->Name = L"ToLabel";
+			this->ToLabel->Size = System::Drawing::Size(20, 13);
+			this->ToLabel->TabIndex = 15;
+			this->ToLabel->Text = L"To";
+			this->ToLabel->Visible = false;
+			// 
 			// mainform
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->AutoSize = true;
 			this->ClientSize = System::Drawing::Size(633, 443);
+			this->Controls->Add(this->NumberEnter);
+			this->Controls->Add(this->ToTextBox);
+			this->Controls->Add(this->FromTextBox);
+			this->Controls->Add(this->RangeNumbers);
 			this->Controls->Add(this->CopyResultButton);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->NumberOfLinesEnter);
 			this->Controls->Add(this->genresult);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->NumberEnter);
 			this->Controls->Add(this->SpecialCharactersCheck);
 			this->Controls->Add(this->NumbersCheck);
 			this->Controls->Add(this->LowerLettersCheck);
 			this->Controls->Add(this->UpperLettersCheck);
 			this->Controls->Add(this->GenerateButton);
+			this->Controls->Add(this->FromLabel);
+			this->Controls->Add(this->ToLabel);
 			this->Name = L"mainform";
 			this->Text = L"Generator Passwords By Art";
 			this->Load += gcnew System::EventHandler(this, &mainform::mainform_Load);
@@ -219,13 +296,41 @@ namespace GeneratorPasswordsByArt {
 #pragma endregion
 	private: System::Void mainform_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-
+	private: System::Int32 choose_rand_number(int from, int to)
+	{
+		
+		vector<int> range;
+		for (int i = from; i <= to; i++)
+		{
+			range.push_back(i);
+		}
+		random_device rd2;
+		mt19937 gen(rd2());
+		uniform_int_distribution<> dist(0, range.size() - 1);
+		return range[dist(gen)];
+	}
 		   
 	private: System::Void GenerateButton_Click(System::Object^ sender, System::EventArgs^ e) {
 
-		if (this->NumberEnter->Text == "" || this->NumberEnter->Text == "0")
+		if (this->RangeNumbers->Checked == false && (this->NumberEnter->Text == "" || this->NumberEnter->Text == "0") )
 		{
 			MessageBox::Show("Please select the number of characters", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			return;
+		}
+		
+		if (this->RangeNumbers->Checked && (this->FromTextBox->Text == ""))
+		{
+			MessageBox::Show("Please enter the number in the field From", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			return;
+		}
+		if (this->RangeNumbers->Checked && (this->ToTextBox->Text == ""))
+		{
+			MessageBox::Show("Please enter the number in the field To", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			return;
+		}
+		if(Convert::ToInt32(this->FromTextBox->Text)> Convert::ToInt32(this->ToTextBox->Text))
+		{
+			MessageBox::Show("From must be lower than To", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
 			return;
 		}
 		if (this->NumberOfLinesEnter->Text == "" || this->NumberOfLinesEnter->Text == "0")
@@ -272,18 +377,39 @@ namespace GeneratorPasswordsByArt {
 		mt19937 gen(rd());                     
 		uniform_int_distribution<> dist(0, rand_range.size()-1); 
 		
-		String^ result = "";
-		for (int j = 0; j < Convert::ToInt32(this->NumberOfLinesEnter->Text); j++)
-		{
-			String^ resik = "";
-			for (int i = 0; i < Convert::ToInt32(this->NumberEnter->Text); i++)
-			{
-				char random_char = rand_range[dist(gen)];
-				resik += (wchar_t)random_char;
-			}
-			result += resik;
-			result += "\r\n";
+		
 
+		String^ result = "";
+
+		if (this->RangeNumbers->Checked)
+		{
+			for (int j = 0; j < Convert::ToInt32(this->NumberOfLinesEnter->Text); j++)
+			{
+				String^ resik = "";
+				for (int i = 0; i < choose_rand_number(Convert::ToInt32(this->FromTextBox->Text), Convert::ToInt32(this->ToTextBox->Text)); i++)
+				{
+					char random_char = rand_range[dist(gen)];
+					resik += (wchar_t)random_char;
+				}
+				result += resik;
+				result += "\r\n";
+
+			}
+		}
+		else if(this->RangeNumbers->Checked == false)
+		{
+			for (int j = 0; j < Convert::ToInt32(this->NumberOfLinesEnter->Text); j++)
+			{
+				String^ resik = "";
+				for (int i = 0; i < Convert::ToInt32(this->NumberEnter->Text); i++)
+				{
+					char random_char = rand_range[dist(gen)];
+					resik += (wchar_t)random_char;
+				}
+				result += resik;
+				result += "\r\n";
+
+			}
 		}
 		result = result->TrimEnd();
 		this->genresult->Text = result;
@@ -307,6 +433,31 @@ private: System::Void CheckDigit(System::Object^ sender, System::Windows::Forms:
 	char ch = e->KeyChar;
 	if  (  (ch < 48 || ch >57) && ch != 8 )
 		e->Handled = true;
+
+}
+	private: System::Void RangeNumbers_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+
+
+
+
+		if (this->RangeNumbers->Checked)
+		{
+			this->FromLabel->Visible = true;
+			this->ToLabel->Visible = true;
+			this->FromTextBox->Visible = true;
+			this->ToTextBox->Visible = true;
+			this->NumberEnter->Visible = false;
+		}
+		else if (this->RangeNumbers->Checked == false)
+		{
+			this->FromLabel->Visible = false;
+			this->ToLabel->Visible = false;
+			this->FromTextBox->Visible = false;
+			this->ToTextBox->Visible = false;
+			this->NumberEnter->Visible = true;
+		}
+
+
 
 }
 };
